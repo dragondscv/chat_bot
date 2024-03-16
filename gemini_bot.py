0b0892f9841d8bb681d2c966a750e8937b244946
dragondscv <dragondscv@gmail.com>
2024-03-15 17:37:56 -0700

    add gemini bot

---
+++ gemini_bot.py
@@ -0,0 +1,22 @@
+import os
+import google.generativeai as genai
+
+
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+while True:
+    prompt = input("Ask me anything: ")
+    if (prompt == "exit"):
+        break
+    response = chat.send_message(prompt, stream=True)
+    for chunk in response:
+        if chunk.text:
+          print(chunk.text)
